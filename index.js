4866829fe8686a7c7cc93b1ea81946ce1789d98d
kirdzaza <148518495+kirdzaza@users.noreply.github.com>
2024-07-06 23:50:57 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function random_color() {
+  var s1 = Math.floor(Math.random() * 256);
+  var s2 = Math.floor(Math.random() * 256);
+  var s3 = Math.floor(Math.random() * 256);
+  if (s1 + s2 + s3 < 200) {
+    $(".col-sm-4").css("color", "white");
+  } else {
+    $(".col-sm-4").css("color", "black");
+  }
+  return `rgb(${s1}, ${s2}, ${s3})`;
+}
+
+$(document).ready(function () {
+  $("#appendButton").click(function () {
+    for (var i = 1; i <= 3; i++) {
+      var newSquare = $("#r" + i);
+      newSquare.css("background-color", random_color());
+      $("#r" + i).text(random_color);
+    }
+  });
+});
